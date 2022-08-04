5702163723fa996f461179420ada4022f9f13e14
shelleymartinez <shelley.martinez@gmail>
2022-08-04 21:14:01 +0000

    Adding a new file named ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
+#---root/ec2.tf---
 
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+module "ec2-instance" {
+  source = "./ec2-instance"
 }
