cb2ef5bf70437bb28df52c32ed4ffc63b5cfb1c0
Herby J <jeanty.herby@gmail.com>
2022-09-28 00:11:33 -0400

    added boot strap

--- ec2.tf
+++ ec2.tf
@@ -17,6 +17,13 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  user_data     = <<EOF
+  #!/bin/bash
+  sudo apt update -y
+  sudo apt install apache2 -y
+  sudo systemctl start apache2
+  sudo systemctl status apache2
+  EOF
 
   tags = {
     Name = "ExampleAppServerInstance"
