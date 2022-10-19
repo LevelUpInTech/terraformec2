630c1b49fe07ac27f64059ef1dac16a92c9cadf1
Ashlee Cooper <ashleedolan1@yahoo.com>
2022-10-19 09:04:45 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  type        = string
+  default     = "ami-026b57f3c383c2eec"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tag" {
+  type        = string
+  default     = "acoop_instance"
+}
