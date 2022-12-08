4acd77b166883d16e8bb5224159552334cb21d30
Cedric Watkins <ced.wat1@gmail.com>
2022-12-08 00:31:20 +0000

    Added modules for ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
+module "ec2_instance" {
+  source = "./instance_module/"
 
-  required_version = ">= 0.14.9"
-}
+  instance_name = var.instance_name
+  ami           = var.ami
+  instance_type = var.instance_type
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
