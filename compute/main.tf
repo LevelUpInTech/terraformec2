75f91c54d1cfdff984555ca3ae0f2d5f8709941e
= <=>
2023-01-07 04:28:03 +0000

    Merge remote-tracking branch 'origin'

--- compute/main.tf
+++ compute/main.tf
@@ -1,10 +1,22 @@
-#----compute main.tf------
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Instanceforwebserver"
+#-----root_ec2/main.tf-----
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
   }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+
+
+module "security" {
+  source = "./module/security"
 }
