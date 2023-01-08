e2c9a61914f7e66267c3883bf20174ff1de435ee
Jeffrey <jeffreycohenit@gmail.com>
2023-01-08 00:24:07 +0000

    Saving my code to my repo

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "JC-App-Server-Instance"
   }
 }
