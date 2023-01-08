a84bb0dde939408ad1bc64e7da87115ebd0985b5
ChamakeNjefi <chamakenjefi@gmail.com>
2023-01-08 15:26:22 +0000

    root & module

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
+# ----root/ec2.tf # ---------
 
-  required_version = ">= 0.14.9"
+module "ec2_instance" {
+  source = "./ec2-instance"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
