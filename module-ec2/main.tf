4706915eb7bacc6871721edcbe5a0b4b23442bff
Joseph Peter <josephvpeter7@gmail.com>
2023-01-11 12:50:05 -0500

    output file created

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "my_app_server" {
   ami                    = var.ami_id
   instance_type          = var.instance
   vpc_security_group_ids = [aws_security_group.allow_http.id]
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id              = aws_subnet.public_subnet.id
   tags = {
     Name = "EC2-App-Server"
   }
@@ -20,9 +20,9 @@ resource "aws_vpc" "myvpc_main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.myvpc_main.id
-  cidr_block = var.cidr
-  availability_zone = var.az
+  vpc_id                  = aws_vpc.myvpc_main.id
+  cidr_block              = var.cidr
+  availability_zone       = var.az
   map_public_ip_on_launch = true
   tags = {
     Name = "public-subnet"
@@ -32,20 +32,20 @@ resource "aws_security_group" "allow_http" {
   name        = "allow-http"
   description = "Allow http inbound traffic"
   vpc_id      = aws_vpc.myvpc_main.id
-  
+
   ingress {
-    description      = "http allowed"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "http allowed"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
