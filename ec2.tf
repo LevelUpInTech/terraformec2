22c25e6fbc253dd0bba66b064e2257f4bc1f03d3
Valentechno <valentino.agbetoglo.study@gmail.com>
2023-06-21 11:40:39 -0400

    ec2 module created

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
+module "my-ec2" {
+  source = "./my-ec2"
 }
